0172c7a4b8bc13a14d79bbfd2135e06ff4264b56
Olayiwola <agbajeolaiwola@gmail.com>
2022-02-16 11:29:37 +0100

    'navbarResponse'

--- components/navbar/Navbar.js
+++ components/navbar/Navbar.js
@@ -1,16 +1,118 @@
-import React from "react";
+import React, { useState, useCallback, useEffect } from "react";
 import styles from '../../styles/navbar/navbar.module.css'
 import Grlogo from "../logo/Grlogo";
 import Link from 'next/link'
 import Lettermarklogo from "../logo/Lettermarklogo";
 import { useRouter } from 'next/router'
+import { slide as Menu } from 'react-burger-menu'
 
 const Navbar = ()=>{
 
-    const router = useRouter()
 
+    const useMediaQuery = (width) => {
+        const [targetReached, setTargetReached] = useState(false);
+      
+        const updateTarget = useCallback((e) => {
+          if (e.matches) {
+            setTargetReached(true);
+          } else {
+            setTargetReached(false);
+          }
+        }, []);
+      
+        useEffect(() => {
+          const media = window.matchMedia(`(max-width: ${width}px)`);
+          media.addListener(updateTarget);
+      
+          // Check on mount (callback is not called until a change occurs)
+          if (media.matches) {
+            setTargetReached(true);
+          }
+      
+          return () => media.removeListener(updateTarget);
+        }, []);
+      
+        return targetReached;
+      };
+      var navstyles = {
+        bmBurgerButton: {
+          position: 'fixed',
+          width: '36px',
+          height: '30px',
+        },
+        bmBurgerBars: {
+          background: '#fff'
+        },
+        bmBurgerBarsHover: {
+          background: '#a90000'
+        },
+        bmCrossButton: {
+          height: '24px',
+          width: '24px'
+        },
+        bmCross: {
+          background: '#bdc3c7'
+        },
+        bmMenuWrap: {
+          position: 'fixed',
+          height: 'fit-content'
+        },
+        bmMenu: {
+          background: '#373a47',
+          padding: '2.5em 1.5em 0',
+          fontSize: '1.15em'
+        },
+        bmMorphShape: {
+          fill: '#373a47'
+        },
+        bmItemList: {
+          color: '#b8b7ad',
+          padding: '0.8em'
+        },
+        bmItem: {
+          display: 'inline-block'
+        },
+        bmOverlay: {
+          background: 'none'
+        }
+      }
+
+      const isBreakpoint = useMediaQuery(2250)
+    const router = useRouter()
     return (
+        
         <div>
+            {
+                isBreakpoint ? 
+
+                <nav className={styles.nav}>
+                <Link href='./'>
+                    <div  style={{cursor:'pointer'}}>
+                        <Grlogo/>
+                    </div>
+                </Link>
+                <Menu 
+                styles={ navstyles}
+                right>
+                    <div>
+                        <div className={styles.navroutes}>
+                            <Link href='./about'>
+                            <div
+                            className={router.pathname == '/about' ? styles.active : styles.notActive}
+                             ><p>ABOUT</p></div>
+                            </Link>
+                            <Link href='./spaces'>
+                            <div 
+                            className={router.pathname == '/spaces' ?styles.active: styles.notActive}><p>SPACES</p></div>
+                            </Link>
+                            {/* <Link href='./partner'>
+                            <div><p>PARTNER WITH US</p></div>
+                            </Link> */}
+                        </div>
+                    </div>
+                </Menu>
+                </nav>
+                :
             <nav className={styles.nav}>
             <Link href='./'>
                 <div  style={{cursor:'pointer'}}>
@@ -26,8 +128,7 @@ const Navbar = ()=>{
                         </Link>
                         <Link href='./spaces'>
                         <div 
-                        className={router.pathname == '/spaces' ?styles.active: styles.notActive}
-                        style={{}}><p>SPACES</p></div>
+                        className={router.pathname == '/spaces' ?styles.active: styles.notActive}><p>SPACES</p></div>
                         </Link>
                         <Link href='./partner'>
                         <div><p>PARTNER WITH US</p></div>
@@ -39,7 +140,7 @@ const Navbar = ()=>{
                     </div>
                 </div>
             </nav>
-
+        }
         </div>
     )
 }
