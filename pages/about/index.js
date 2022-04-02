a930a056a4e49f943683bf8661c906a98bec0e5a
agbajeolayiwola <agbajeolaiwola@gmail.com>
2022-04-02 01:58:40 +0100

    'sticky'

--- pages/about/index.js
+++ pages/about/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Abstract from "../../components/anim/abstract";
+import AbstractI from "../../components/anim/abstractI";
 
 const About = () =>{
 
     return(
         <div>
             <Abstract/>
+            <AbstractI/>
         </div>
     )
 }
