259abf173ff64b89bc7b8caeea928eca28987447
agbajeolayiwola <agbajeolaiwola@gmail.com>
2022-04-27 13:46:16 +0100

    'reqbtn'

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,17 @@
 import Layout from '../components/Layout'
 import '../styles/globals.css'
+import Head from 'next/head'
+
 
 
 function MyApp({ Component, pageProps }) {
 
   return( 
   <Layout>
+    <Head>
+      <title>Geek Radius</title>
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+    </Head>
     <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet"/>
           <link href="https://fonts.googleapis.com/css2?family=Mulish:wght@200;400&family=Playfair+Display&display=swap" rel="stylesheet"/>
           
