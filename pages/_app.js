48547de1b01daf8e3074ed1e2fa2158ac19471a1
agbajeolayiwola <agbajeolaiwola@gmail.com>
2022-04-27 13:58:44 +0100

    'reqbtn'

--- pages/_app.js
+++ pages/_app.js
@@ -10,10 +10,11 @@ function MyApp({ Component, pageProps }) {
   <Layout>
     <Head>
       <title>Geek Radius</title>
+      <link rel="shortcut icon" href="/vercel.svg" />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
     </Head>
     <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet"/>
-          <link href="https://fonts.googleapis.com/css2?family=Mulish:wght@200;400&family=Playfair+Display&display=swap" rel="stylesheet"/>
+    <link href="https://fonts.googleapis.com/css2?family=Mulish:wght@200;400&family=Playfair+Display&display=swap" rel="stylesheet"/>
           
        
   <Component {...pageProps} />
